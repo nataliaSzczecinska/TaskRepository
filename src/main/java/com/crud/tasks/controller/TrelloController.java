51339b3d7e4e522704ddb286ed507dc527100e58
nataliaSzczecinska <nataliaszczecinska@o2.pl>
2021-03-18 21:55:22 +0100

    23.1 heroku

--- src/main/java/com/crud/tasks/controller/TrelloController.java
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.*;
 import java.util.stream.*;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/v1/trello")
 @RequiredArgsConstructor
